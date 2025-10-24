208d1be122ad7dfc0b866d997a3e203a346c990f
Milan <mp09092003@gmail.com>
2025-10-24 22:04:33 +0200

    Category service met interface gemaakt

---
+++ Grocery.Core/Interfaces/Services/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Grocery.Core.Models;
+
+namespace Grocery.Core.Interfaces.Services
+{
+	public interface ICategoryService
+	{
+		public List<Category> GetAll();
+		public Category? Get(int id);
+	}
+}
---
+++ Grocery.Core/Services/CategoryService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Grocery.Core.Interfaces.Repositories;
+using Grocery.Core.Interfaces.Services;
+using Grocery.Core.Models;
+
+namespace Grocery.Core.Services
+{
+	public class CategoryService : ICategoryService
+    {
+		private readonly ICategoryRepository _CategoryRepository;
+
+		// Dependency Injection van CategoryRepository 
+		public CategoryService(ICategoryRepository categoryRepository)
+		{
+			_CategoryRepository = categoryRepository;
+		}
+
+		public List<Category> GetAll()
+		{
+			return _CategoryRepository.GetAll();
+		}
+
+		public Category? Get(int id)
+		{
+			return _CategoryRepository.Get();
+		}
+	}
+}
+
+
