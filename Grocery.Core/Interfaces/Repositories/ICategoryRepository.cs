344c45202db0aedaedba8c58864a8acfc4736822
Milan <mp09092003@gmail.com>
2025-10-24 21:13:55 +0200

    Interface voor CategoryRepository gemaakt

---
+++ Grocery.Core/Interfaces/Repositories/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Grocery.Core.Models;
+
+namespace Grocery.Core.Interfaces.Repositories
+{
+	public interface ICategoryRepository
+	{
+		public ICategoryRepository()
+		{
+			public List<Category> GetAll();	// GetAll() geeft lijst met Category objecten
+			public Category? Get(int id);	// Kan een Category meegeven of Null
+		}
+	}
+}
+
