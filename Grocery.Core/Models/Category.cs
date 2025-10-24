6a77abe7de327ec8971ba88117218ac05c5abc98
Milan <mp09092003@gmail.com>
2025-10-24 21:13:34 +0200

    category class gemaakt

---
+++ Grocery.Core/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Grocery.Core.Models
+{
+	public class Category : Model
+	{
+		public Category(int id, string name) : base(id, name)
+		{
+			// property definiëren is al gebeurd in 'Model'
+		}
+	}
+}
+
