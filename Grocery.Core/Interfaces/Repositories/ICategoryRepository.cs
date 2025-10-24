3b051d4223ae22d1be987f55fd12804827cdedd5
Milan <mp09092003@gmail.com>
2025-10-24 22:25:40 +0200

    syntax fout gefixed

--- Grocery.Core/Interfaces/Repositories/ICategoryRepository.cs
+++ Grocery.Core/Interfaces/Repositories/ICategoryRepository.cs
@@ -5,11 +5,8 @@ namespace Grocery.Core.Interfaces.Repositories
 {
 	public interface ICategoryRepository
 	{
-		public ICategoryRepository()
-		{
-			public List<Category> GetAll();	// GetAll() geeft lijst met Category objecten
-			public Category? Get(int id);	// Kan een Category meegeven of Null
-		}
+		public List<Category> GetAll();	// GetAll() geeft lijst met Category objecten
+		public Category? Get(int id);	// Kan een Category meegeven of Null
 	}
 }
 
