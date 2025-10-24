4106bfe7036eb0048baaf2b6f2ae7abf9e98f3b4
Milan <mp09092003@gmail.com>
2025-10-24 21:14:16 +0200

    Categoryrepository gemaakt met data

---
+++ Grocery.Core.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Grocery.Core.Data
+{
+	public class CategoryRepository : ICategoryRepository
+	{
+		private List<Category> categories;
+		public CategoryRepository()
+		{
+			categories = [
+				new Category(1, "zuivel"),
+				new Category(2, "ontbijt"),
+				new Category(3, "graanproducten")
+				new Category(4, "vlees"),
+				new Category(5, "snacks"),
+				new Category(6, "drinken")
+				];
+		}
+
+		public List<Category> GetAll()
+		{
+			return categories;	// Geef lijst met alle categories terug
+		}
+
+		public Category? Get(int id)
+		{
+			foreach (category in categories)
+			{
+				// Check of de id klopt met de id van de category
+				if (category.Id == id)
+				{
+					return Category;
+				}
+			}
+
+			return null;	// geef null als er geen category gevonden is
+		}
+	}
+}
+
